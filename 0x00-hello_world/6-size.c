ea22e9a3e48c2e23feffb0853f69a655d6ac920c
electraestella <crystalnkirukamadu@gmail.com>
2023-10-05 15:04:13 -0700

    My 6-size.c commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,4 +3,17 @@
  * main - A fuction that outputs the sizeof various computer data types
  * Return: 0 (Upon successful execution)
  */
-
+int main(void)
+{
+char m;
+int n;
+long int o;
+long long int p;
+float q;
+printf("Size of a char: %lu byte(s)\n", sizeof(m));
+printf("Size of int: %lu byte(s)\n", sizeof(n));
+printf("Size of long int: %lu byte(s)\n", sizeof(o));
+printf("Size of long long int: %lu byte(s)\n", sizeof(p));
+printf("Size of float: %lu byte(s)\n", sizeof(q));
+return (0);
+}

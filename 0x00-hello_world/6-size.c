ff0997407d6f742b15264b7258cdd29c9b606c5f
electraestella <crystalnkirukamadu@gmail.com>
2023-10-05 15:42:40 -0700

    My 6-size commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ long int o;
 long long int p;
 float q;
 printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(m));
-printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(n));
-printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(o));
-printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(p));
-printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(q));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(n));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(o));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(p));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(q));
 return (0);
 }

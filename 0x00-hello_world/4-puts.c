0a551270e7d97bc46fe642edb37ba70e1b6eeca2
electraestella <crystalnkirukamadu@gmail.com>
2023-10-05 13:37:27 -0700

    My 4-puts.c commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A funtion that outputs a line with puts function
+ * Return: 0(Upon successful execution)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

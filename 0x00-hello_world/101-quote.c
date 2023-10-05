47c9043c63508588930659569bd097a7fb1c4f89
electraestella <crystalnkirukamadu@gmail.com>
2023-10-05 16:04:05 -0700

    My 101-quote.c commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - A funtion that outputs a line of text to standard error
+ * Return: 1(Upon successful execution)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}

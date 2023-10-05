f03e073dfe88137ab1160396ceadaccd6aa32dc7
electraestella <crystalnkirukamadu@gmail.com>
2023-10-05 13:51:45 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A funtion that outputs a line of text using printf function
+ * Return: 0 (Upon successful execution)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}

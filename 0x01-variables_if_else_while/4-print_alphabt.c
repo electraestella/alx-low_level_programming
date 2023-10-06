4d296cc85ff18118f2a0fe1137f18f758cfadd8e
electraestella <crystalnkirukamadu@gmail.com>
2023-10-06 15:45:45 -0700

    My 4-print_alphabt.c commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - The entry point of the program
+ * Return: 0 (Upon successful execution)
+ */
+int main(void)
+{
+char X;
+x = 'a';
+while (x <= 'z')
+{
+if ((x != 'q' && x != 'e') && x <= 'z')
+putchar (x);
+x++;
+}
+putchar ('\n');
+return (0);
+}

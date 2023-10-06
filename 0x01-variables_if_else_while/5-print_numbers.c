997f01f58ca0b2502299d05a0613863965c6fd01
electraestella <crystalnkirukamadu@gmail.com>
2023-10-06 15:54:24 -0700

    My 5-print_numbers.c commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - The entry point of the program
+ * Return: 0 (Upon successsful execution)
+ */
+int main(void)
+{
+int x;
+for (x = o, x <= 9; x++)
+printf("%d", x);
+putchar ('\n');
+return (0);
+}
+

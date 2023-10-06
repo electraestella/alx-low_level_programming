3f591dfe9c08ca659c0dc0de86b1a55543103915
electraestella <crystalnkirukamadu@gmail.com>
2023-10-06 14:24:57 -0700

    My 2-print_alphabet.c commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A function for the entry point
+ * Return: 0 (Upon successful execution)
+ */
+int main(void)
+{
+char b;
+b = 'a';
+while (b <= 'z')
+{
+putchar(b);
+b++;
+}
+putchar('\n');
+return(0);
+}

73ffee24c0c6ec2d298313a0204a3e8ea530e076
electraestella <crystalnkirukamadu@gmail.com>
2023-10-06 17:58:01 -0700

    My 100-print_comb3.c commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - The entry point of the program
+ * Return: 0 (Upon successful execution)
+ */
+int main(void)
+{
+int x;
+int y;
+while (x < '10')
+{
+while (y < '10')
+{
+if (!(x > y) || x == y)
+{
+putchar(x);
+putchar(y);
+if (x == '8' && y == '9')
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+y++;
+}
+y = '0';
+x++;
+}
+return (0)
+}

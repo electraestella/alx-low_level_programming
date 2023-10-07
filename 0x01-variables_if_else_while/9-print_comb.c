431735344786da97c2161b962388b0f63413d627
electraestella <crystalnkirukamadu@gmail.com>
2023-10-06 17:37:20 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - The entry point of the program
+ * Return: 0 (Upon successful execution)
+ */
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+putchar(x + '0');
+if (a < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
